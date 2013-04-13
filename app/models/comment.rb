88a81fcaf196a85bccdda56dc59cc6fc706caace
Hobo86 <hobo.chb@gmail.com>
2013-04-13 13:56:53 +0800

    Commen验证

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  attr_accessible :body, :commenter
+  attr_accessible :body,  :commenter
+	validates :commenter,   :presence => true,
+          			          :length => { :minimum => 4 }
+  validates :body,  :presence => true,
+            			  :length => { :minimum => 2 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   	attr_accessible :content, :name, :title, :tags_attributes
-	validates :name,  	:presence => true
+	  validates :name,  	:presence => true
   	validates :title, 	:presence => true,
-            			:length => { :minimum => 5 }
+            			      :length => { :minimum => 5 }
     has_many :comments, :dependent => :destroy
     has_many :tags
   	accepts_nested_attributes_for :tags, :allow_destroy => :true,
