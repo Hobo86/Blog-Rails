7f32db5c7416e219d7e838df01b344e857c7fcbd
Hobo86 <hobo.chb@gmail.com>
2013-06-20 16:05:02 +0800

    修改archives界面

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -5,15 +5,30 @@ class ArchivesController < ApplicationController
     #fields = "a.*,DATE_FORMAT(a.created_at, '%Y-%m') as month"
     #@posts = Post.from("posts a").joins(join_str).select(fields).where("b.id is NULL").order("created_at DESC")
     
-    fields = "id,DATE_FORMAT(created_at, '%Y-%m') as month"
-    @archives = Post.order("created_at DESC").select(fields)
+    fields = "id,title,content,created_at,DATE_FORMAT(created_at, '%Y-%m') as month"
+    @posts = Post.order("created_at DESC").select(fields)
     
-    @archives.each do |archive|
-      @posts = archive.archive_posts
-      archive[:posts] = @posts
+    @archives = Array.new
+    @posts.each do |post|
+      @month = post[:month]
+      @archive
+      if @archives.include?(@month)
+        @archive = @archives[@month]
+        @archive_posts = @archive[:posts]
+      else
+        @archive = Hash.new
+        @archives.push(@archive)
+        
+        @archive_posts = Array.new
+        @archive[:month] = @month
+        @archive[:posts] = @archive_posts
+      end
+      
+      @archive_posts.push(post)
     end
+    
     respond_to do |format|
-      format.html # admin.html.erb
+      format.html # index.html.erb
       format.json { render json: @archives }
     end
   end
@@ -23,7 +38,7 @@ class ArchivesController < ApplicationController
     @posts = Post.where("DATE_FORMAT(created_at, '%Y-%m') = ?", @archive).order("created_at DESC,id DESC")
     
     respond_to do |format|
-      format.html # admin.html.erb
+      format.html # show.html.erb
       format.json { render json: @posts }
     end
   end
