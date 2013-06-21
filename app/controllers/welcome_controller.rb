667a46d5674ca4db4e4977ca45fe1165e04916ed
Hobo86 <hobo.chb@gmail.com>
2013-06-21 16:43:19 +0800

    修改icon

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   skip_before_filter :require_login, :only => [:index]
   def index
-    @posts = Post.all
+    @posts = Post.limit(10)
 
     respond_to do |format|
       format.html # show.html.erb
